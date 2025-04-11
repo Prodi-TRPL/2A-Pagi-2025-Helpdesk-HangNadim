1cf4486c563a90f9eb8e3262616cae50f0765e63
mhaqqi1234 <mhaqqi1234@gmail.com>
2025-04-11 15:13:18 +0700

    Proses Tambah Students

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/student', [StudentController::class, 'index'])
+    ->name('student.index');
+
+Route::get('/student/add', [StudentController::class, 'create'])
+    ->name('student.create');
+
+Route::POST('/student/add', [StudentController::class, 'store'])
+    ->name('student.store');
