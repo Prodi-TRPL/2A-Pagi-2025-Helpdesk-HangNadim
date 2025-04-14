823e4971b776b066514c69fc6b50f1139de1981e
gabriell74 <gabrielvivaldi07@gmail.com>
2025-04-14 16:50:48 +0700

    Proses Edit Students

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,9 @@ use Illuminate\Support\Facades\Route;use App\Http\Controllers\StudentController;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/student/edit/{id}', [StudentController::class, 'edit'])
+    ->name('student.edit');
+
+Route::put('/student/edit/{id}', [StudentController::class, 'update'])
+    ->name('student.update');
