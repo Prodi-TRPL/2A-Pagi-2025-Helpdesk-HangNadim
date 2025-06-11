5330fd47357e46f024d2ab6774f21eb3c3726d7f
BerlianPIJ <berlianptristnjingga@gmail.com>
2025-06-11 11:14:19 +0700

    penambahan lacak komplain

--- resources/views/public/lacak_komplain.blade.php
+++ resources/views/public/lacak_komplain.blade.php
@@ -1,3 +1,10 @@
 @extends('layout.app')
+
 @section('content')
+<div class="container d-flex flex-column justify-content-center align-items-center" style="min-height: 70vh;">
+    <h1 class="mb-4">Pelacakan Status Aduan</h1>
+    <input type="text" class="form-control rounded-pill w-50" placeholder="Masukkan no tiket">
+    <img src="{{ asset('search.svg') }}" alt="App Preview" class="img-fluid rounded shadow my-5">
+
+</div>
 @endsection
